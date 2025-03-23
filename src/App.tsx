73c9bf922c7016c0d5b890367efa97a8f755f3e6
asdqwedfsa <kdima4615@gmail.com>
2025-03-23 23:51:11 +0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Cart from './pages/Cart';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-white">
+        <Navbar />
+        <AnimatePresence mode="wait">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalog" element={<Catalog />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </AnimatePresence>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
